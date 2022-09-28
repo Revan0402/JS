2dab363c47af30087d24e9f1a0c80c14c3f8b894
Revan0402 <ralizade198@gmail.com>
2022-09-28 11:11:42 +0400

    Tapsiriq

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var a = ("Rəvan")
+var b = ("Alızadə-dir")
+var c = 27
+var d = 02
+var e = 2005
+var f = ("qaradır")
+var g = ("Teen Wolf-dur")
+var h = 2022
+var x = 2005
+var i = ("proqramçı")
+
+
+
+document.getElementsByClassName("tap")[0].innerHTML = (`Mənim adım ${a},soyadım isə ${b}.Mən ${c}.${d}.${e} tarixində dünyaya gəlmişəm.Mənim sevdiyim rəng ${f}.Ən sevdiyim film ${g}.Hal hazırda ${h - x} yaşım var.Mən ${i} olacam.`)
+
+
+
+/*document.getElementsByClassName("tap")[0].innerHTML = ("Mənim adım"+" "+a+","+"soyadım isə"+" "+b+"."+"Mən"+" "+c+"."+d+"."+e+" "+"tarixində dünyaya gəlmişəm."+
+"Mənim sevdiyim rəng"+" "+f+"."+"Ən sevdiyim film"+" "+g+"."+"Hal hazırda"+" "+(h-x)+" "+"yaşım var."+"Mən"+" "+i+" "+"olacam.")*/
